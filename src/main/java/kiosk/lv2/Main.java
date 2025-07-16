2b55ea036ba61b530f453b0e97c48150765ef64e
soo59599 <sooh59599@gmail.com>
2025-07-16 19:18:28 +0900

    feat: #3 MenuItem 클래스 및 필드 초기 정의

---
+++ src/main/java/kiosk/lv2/Main.java
@@ -0,0 +1,12 @@
+package kiosk.lv2;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
