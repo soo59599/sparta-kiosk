c870800402a3ba0e0a10d6e2aa2d8bae76115903
soo59599 <sooh59599@gmail.com>
2025-07-17 13:54:00 +0900

    feat : #5 Kiosk클래스 내 menuItem리스트 필드 생성, start 함수 생성

--- src/main/java/kiosk/lv3/Kiosk.java
+++ src/main/java/kiosk/lv3/Kiosk.java
@@ -3,6 +3,7 @@ package kiosk.lv3;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Kiosk {
 
@@ -14,5 +15,47 @@ public class Kiosk {
         menuItems.add(new MenuItem("Cheeseburger", BigDecimal.valueOf(6.9), "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", BigDecimal.valueOf(5.4), "비프패티를 기반으로 야채가 들어간 기본버거"));
 
+    }
+
+    void start(){
+        Scanner sc = new Scanner(System.in);
+
+        outer:
+        while (true) {
+
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (MenuItem menuItem : menuItems) {
+                System.out.println(menuItem.name + "| w " + menuItem.price + "|" + menuItem.description);
+            }
+
+            try {
+                int choose = sc.nextInt();
+
+                switch (choose) {
+                    case 1:
+                        System.out.println("ShackBurger 주문이 되었습니다.");
+                        break;
+                    case 2:
+                        System.out.println("SmokeShack 주문이 되었습니다.");
+                        break;
+                    case 3:
+                        System.out.println("Cheeseburger 주문이 되었습니다.");
+                        break;
+                    case 4:
+                        System.out.println("Hamburger 주문이 되었습니다.");
+                        break;
+                    case 0:
+                        System.out.println("프로그램을 종료합니다.");
+                        break outer;
+
+                    default:
+                        throw new IllegalArgumentException("0~4번중에 입력하세요");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+        sc.close();
     }
 }
