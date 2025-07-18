a34ee898765cf69e237cd471ac8b62ad45986ee8
soo59599 <sooh59599@gmail.com>
2025-07-18 19:43:11 +0900

    feat: #9 lv5 프로젝트 초기 구조 생성

---
+++ src/main/java/kiosk/lv5/MenuItem.java
@@ -0,0 +1,27 @@
+package kiosk.lv5;
+
+import java.math.BigDecimal;
+
+public class MenuItem {
+    String name;
+    BigDecimal price;
+    String description;
+
+    public  MenuItem(String name, BigDecimal price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
