54703e46381446ae665fae2e85a5daee130e51ed
soo59599 <sooh59599@gmail.com>
2025-07-18 11:33:07 +0900

    feat: #5 Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입

--- src/main/java/kiosk/lv3/Kiosk.java
+++ src/main/java/kiosk/lv3/Kiosk.java
@@ -1,6 +1,5 @@
 package kiosk.lv3;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,12 +8,8 @@ public class Kiosk {
 
     List<MenuItem> menuItems = new ArrayList<>();
 
-    public Kiosk() {
-        menuItems.add(new MenuItem("ShackBurger", BigDecimal.valueOf(6.9), "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", BigDecimal.valueOf(8.9), "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", BigDecimal.valueOf(6.9), "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", BigDecimal.valueOf(5.4), "비프패티를 기반으로 야채가 들어간 기본버거"));
-
+    public Kiosk(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
     }
 
     void start(){
