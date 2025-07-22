b7008edf868e4a3da7faccd4d3a74b067683b4a3
soo59599 <sooh59599@gmail.com>
2025-07-22 20:28:43 +0900

    feat: #14 주석 추가 및 아이템 ì 이름으로 아이템 전부 빼기 메서드 추가

--- src/main/java/kiosk/ch2/Cart.java
+++ src/main/java/kiosk/ch2/Cart.java
@@ -11,7 +11,7 @@ public class Cart {
     private List<CartItem> cartItems;
 
     //생성자
-    public Cart(){
+    public Cart() {
         cartItems = new ArrayList<>();
     }
 
@@ -20,28 +20,45 @@ public class Cart {
         return cartItems.stream().collect(Collectors.toList());
     }
 
-    //아이템 넣기
+    //아이템 몇개 넣기(Id 기준)
     public void increaseItemQuantity(CartItem cartItemToAdd) {
         cartItems.stream().filter(item -> item.getMenuItemId().equals(cartItemToAdd.getMenuItemId()))
                 .findFirst()
-                .ifPresentOrElse(foundItem -> foundItem.increaseQuantity(cartItemToAdd.getQuantity()),()-> cartItems.add(cartItemToAdd));
+                .ifPresentOrElse(foundItem -> foundItem.increaseQuantity(cartItemToAdd.getQuantity()), () -> cartItems.add(cartItemToAdd));
     }
 
-    //아이템 빼기
+    //아이템 몇개 빼기(Id 기준)
     public void decreaseItemQuantity(CartItem cartItemToDecrease) {
         cartItems.stream()
                 .filter(item -> item.getMenuItemId().equals(cartItemToDecrease.getMenuItemId()))
                 .findFirst()
-                .ifPresentOrElse(foundItem->{
-                    foundItem.decreaseQuantity(cartItemToDecrease.getQuantity());
-                    if(foundItem.getQuantity()<=0){
-                        cartItems.remove(foundItem);
+                .ifPresentOrElse(foundItem -> {
+                    foundItem.decreaseQuantity(cartItemToDecrease.getQuantity()); //수량 감소
+
+                    if (foundItem.getQuantity() <= 0) {
+                        cartItems.remove(foundItem); //수량이 0 이하가 되면 장바구니에서 제거
                     }
-                }, ()-> System.out.println("해당 아이템이 카트에 없습니다."));
+
+
+                }, () -> System.out.println("해당 아이템이 카트에 없습니다.")); //찾는 항목이 없을 때
+    }
+
+    //아이템 전부 빼기(name 기준)
+    public void deleteItem(CartItem cartItemToDelete) {
+        //필터로 새 리스트를 만들어서 반환
+//        cartItems = cartItems.stream()
+//                .filter(item -> !item.getMenuItemName().equals(cartItemToDelete.getMenuItemName()))
+//                .collect(Collectors.toList());
+
+//        //removeIf활용
+//        cartItems.removeIf(item -> item.getMenuItemName().equals(cartItemToDelete.getMenuItemName()));
+
+        // 동일 이름의 아이템은 카트에 하나만 담겨있음->하나만 제거해도 모두 제거됨
+        cartItems.stream().filter(item -> item.getMenuItemName().equals(cartItemToDelete.getMenuItemName())).findFirst().ifPresent(cartItems::remove);
     }
 
     //카트에 담긴 총 금액 확인
-    public BigDecimal getCartTotalPrice(){
+    public BigDecimal getCartTotalPrice() {
         return cartItems.stream()
                 .map(CartItem::getTotalPrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
--- src/main/java/kiosk/ch2/Kiosk.java
+++ src/main/java/kiosk/ch2/Kiosk.java
@@ -28,16 +28,17 @@ public class Kiosk {
                 }
 
                 //첫번째 선택:메인에서 카테고리 고르기 (인덱스 편의를 위해 -1)
-                //사용자 입력: 메인 메뉴에서 카테고리 선택 (1~3: 카테고리, 4: 주문, 5: 취소, 0: 종료)
+                //메인 메뉴에서 카테고리 선택 (1~3: 카테고리, 4: 주문, 5: 장바구니 비우기, 0: 종료)
                 int selectedCategoryIndex = sc.nextInt() - 1;
 
-                //메인메뉴에서 종료 선택
+                //메인메뉴에서 종료(0) 선택
                 if (selectedCategoryIndex == -1) {
                     System.out.println("프로그램을 종료합니다.");
                     break;
                 }
 
                 //선택한 카테고리의 아이템 메뉴 출력
+                //메인메뉴에서 카테고리(1~3) 선택
                 if (-1 < selectedCategoryIndex && selectedCategoryIndex < 3) {
                     Menu chosenMenu = menus.get(selectedCategoryIndex);
                     displayCategoryMenu(chosenMenu);
@@ -54,19 +55,21 @@ public class Kiosk {
                     if (-1 < itemChoice && itemChoice < chosenMenu.getMenuItems().size()) {
                         MenuItem chosenItem = chosenMenu.getMenuItems().get(itemChoice);
                         System.out.printf("선택한 메뉴: %s | W %.1f | %s%n", chosenItem.getName(), chosenItem.getPrice(), chosenItem.getDescription());
-
-                        //선택한 아이템 장바구니 추가
                         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
                         System.out.println("1. 확인        2. 취소");
 
                         //세번째 선택: 장바구니 담기
                         int cartItemChoice = sc.nextInt();
 
+                        //선택한 아이템 장바구니 추가
                         if (cartItemChoice == 1) {
                             System.out.println(chosenItem.getName() + " 이 장바구니에 추가되었습니다.");
                             cart.increaseItemQuantity(new CartItem(chosenItem, 1));
+
                         } else if (cartItemChoice == 2) {
+                            //장바구니 담기에서 뒤로가기 선택
                             System.out.println("메인 메뉴로 돌아갑니다.");
+
                         } else {
                             throw new IllegalArgumentException("잘못된 입력입니다.");
                         }
@@ -75,28 +78,42 @@ public class Kiosk {
                         throw new IllegalArgumentException("잘못된 입력입니다.");
                     }
 
+                //메인메뉴에서 주문(4) 선택
                 } else if (!cart.getCartItems().isEmpty() && selectedCategoryIndex == 3) {
                     displayCart();
                     System.out.println("1. 주문      2. 메뉴판");
+
+                    //네번째 선택: 주문하기
                     int orderChoice = sc.nextInt();
                     sc.nextLine(); //버퍼 지우기
+
+                    //주문하기
                     if (orderChoice == 1) {
                         displayDiscountRateMenu();
+
+                        //다섯번째 선택: UserType 선택
                         int userTypeChoice = sc.nextInt();
                         sc.nextLine(); //버퍼 지우기
+
+                        //주문 완료
                         UserType userType = UserType.values()[userTypeChoice - 1];
                         System.out.println("주문이 완료되었습니다. 금액은 W " + userType.getDiscountedPrice(cart.getCartTotalPrice()) + " 입니다.");
                         cart.cartClear();
+
+                    //주문하기에서 뒤로가기
                     } else if (orderChoice == 2) {
                         System.out.println("메뉴판으로 돌아갑니다.");
                     }
 
+                //메인메뉴에서 장바구니 비우기(5) 선택
                 } else if (!cart.getCartItems().isEmpty() && selectedCategoryIndex == 4) {
                     cart.cartClear();
                     System.out.println("장바구니를 비웠습니다.");
+
                 } else {
                     throw new IllegalArgumentException("잘못된 입력입니다.");
                 }
+
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             } catch (InputMismatchException e) {
@@ -152,8 +169,8 @@ public class Kiosk {
     //할인정보 출력
     public void displayDiscountRateMenu() {
         System.out.println("할인 정보를 입력해주세요.");
-        for(int i = 0 ; i <UserType.values().length ; i++){
-            System.out.println((i + 1) + ". " + UserType.values()[i].getDisplayName()+" : "+UserType.values()[i].getDiscountPercent()+"%");
+        for (int i = 0; i < UserType.values().length; i++) {
+            System.out.println((i + 1) + ". " + UserType.values()[i].getDisplayName() + " : " + UserType.values()[i].getDiscountPercent() + "%");
         }
     }
 }
